a8ac5ae5bbf75314b3c444dde2e9d357a7ee33b9
Matthew Gonzalez <matthewgonzalez7725@gmail.com>
2024-05-27 15:15:06 -0400

    added reset.css and updated js and index html files

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,28 @@
 var apiKey="c4f281eb19c7ac7ac64ab55e16d2f4b5"
 var dashboardEl=document.getElementById("dashboard")
+var fiveDayEl=document.getElementById("five-day-forecast")
+var searchInputEl=document.getElementById("search-input")
+var searchBtnEl=document.getElementById("search-btn")
+var historyBtnEl=document.getElementById("history-btn")
+var historyArr=JSON.parse(localStorage.getItem("history")) || []
+
+displayHistory()
+
+function displayHistory(){
+    historyBtnEl.innerHTML=""
+    for(var i=0; i<historyArr.length; i++){
+        historyBtnEl.innerHTML=historyBtnEl.innerHTML+`<button type="button" class="btn bg-secondary w-100 mx-2 my-1">${historyArr[i]}</button>`
+    }
+}
+
+function populateData(event){
+    var currentButton=event.target
+    var cityName=currentButton.textContent
+    getCurrentWeather(cityName)
+    fiveDayForecast(cityName)
+}
+
+historyBtnEl.addEventListener("click", populateData)
 
 function getCurrentWeather(cityName){
     var url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
@@ -11,15 +34,22 @@ function getCurrentWeather(cityName){
     .then(function(data){
         console.log(data)
 
+        if(historyArr.includes(data.name)===false){
+            historyArr.push(data.name)
+
+            localStorage.setItem("history", JSON.stringify(historyArr))
+            displayHistory()
+        }
+
         dashboardEl.innerHTML=`<h3>${data.name} (${dayjs.unix(data.dt).format("MM/DD/YYYY")}) <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt=""></h3>
-        <p>Temperature: 85 F</p>
-        <p>Wind: 7.7 MPH</p>
-        <p>Humidity: 51%</p>`
+        <p>Temperature: ${data.main.temp} F</p>
+        <p>Wind: ${data.wind.speed} MPH</p>
+        <p>Humidity: ${data.main.humidity}%</p>`
     })
 }
 
 function fiveDayForecast(cityName){
-    var url=`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+    var url=`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`
 
     fetch(url)
     .then(function(response){
@@ -27,14 +57,58 @@ function fiveDayForecast(cityName){
     })
     .then(function(data){
         console.log(data)
+        fiveDayEl.textContent=""
 
         for(var i=3; i<data.list.length; i=i+8){
+
             var fiveDayArray=data.list
             console.log(fiveDayArray[i])
+            
+            var divCol=document.createElement("div")
+            divCol.classList="col-sm-2 mb-3 mb-sm-0"
+
+            var divCard=document.createElement("div")
+            divCard.classList="card"
+
+            var divBody=document.createElement("div")
+            divBody.classList="card-body"
+
+            var h5=document.createElement("h5")
+            h5.classList="card-title"
+            h5.textContent=dayjs.unix(fiveDayArray[i].dt).format("MM/DD/YYYY")
+            divBody.appendChild(h5)
+
+            var img=document.createElement("img")
+            img.src="https://openweathermap.org/img/wn/"+fiveDayArray[i].weather[0].icon+"@2x.png"
+            divBody.appendChild(img)
+
+            var pTemp=document.createElement("p")
+            pTemp.classList="card-text"
+            pTemp.textContent=" temp: "+fiveDayArray[i].main.temp
+            divBody.appendChild(pTemp)
+            divCard.appendChild(divBody)
+            divCol.appendChild(divCard)
+
+            fiveDayEl.appendChild(divCol)
+
 
         }
     })
 }
 
-getCurrentWeather('Miami')
-fiveDayForecast('Miami')
+
+
+function search(){
+    var cityName=searchInputEl.value
+    if(cityName){
+        getCurrentWeather(cityName)
+        fiveDayForecast(cityName)
+    }else{
+        alert("Please enter the name of a city.")
+    }
+    
+}
+
+searchBtnEl.addEventListener("click", search)
+
+displayHistory()
